fae0f3321218e4cf4f78990f250698227e3e3c01
87saku39 <87saku39>
2019-06-07 22:54:53 +0900

    タイマータスク追加し 時間経過とともにストック数が減少するようにした。

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,36 +22,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         Button button=findViewById(R.id.button3);//ボタンの設定
-        TextView textView2=findViewById(R.id.textView);
-        final SharedPreferences[] datastore = new SharedPreferences[1];
-
-
+        TextView textView2=findViewById(R.id.textView);//"シャンプーのストック"
+        final SharedPreferences[] datastore = new SharedPreferences[1];//データ保存先
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText=(EditText)findViewById(R.id.editText);//入力値
-                SharedPreferences preferences = getSharedPreferences("storedata",MODE_PRIVATE);//データ保存
+                EditText editText = (EditText) findViewById(R.id.editText);//入力値
+                SharedPreferences preferences = getSharedPreferences("storedata", MODE_PRIVATE);
+                //データ保存
 
 
-                int a = preferences.getInt("count" , 0);;
+                int a = preferences.getInt("count", 0);
                 int b = Integer.parseInt(editText.getText().toString());
-                int c = a+b;
-
-
-//ストック数保存
+                final int[] c = {a + b};//ストック数
                 SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt("count",c);
+                editor.putInt("count", c[0]);//保存先にデータ追加
                 editor.commit();
 
-                TextView textView1 = (TextView)findViewById(R.id.textView);
-                textView1.setText(String.valueOf(c));
 
+
+                Timer timer;
+                timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        c[0] =c[0]-2;
+                        TextView textView1 = (TextView) findViewById(R.id.textView);
+                        textView1.setText(String.valueOf(c[0]));
+                    }
+                };
+                timer.scheduleAtFixedRate(timerTask, 0,     10000);
+
+
+
+                
 
 
             }
 
-
         });
+
+
+
+
     }
+
 }
